0a83e9d77feaed4f5681acdfbdc9fb0b0de634ce
omerf9 <omeryousif.dev@gmail.com>
2026-07-31 23:26:56 -0400

    Point frontend to live backend for deployment

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
 // src/api/config.js central place for the backend URL
-export const API_URL = 'http://localhost:3000/api';
-//export const API_URL = 'https://portfolio-backend-qd3e.onrender.com/api';
+//export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://portfolio-backend-qd3e.onrender.com/api';
